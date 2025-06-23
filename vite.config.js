87cf5e0b684029f3069ab5cebdd5b495d316d246
Saad-007 <saadsafeer223@gmail.com>
2025-06-23 20:51:52 +0500

    Remove base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/zu-project/', // <-- IMPORTANT for GitHub Pages
   plugins: [
     tailwindcss(),
     react()
